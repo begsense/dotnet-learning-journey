e6de78fad889a99ce605336c42d7811974c0e8c6
begsense <begsense@gmail.com>
2025-09-17 22:03:50 +0400

    add lecture3,2 working

---
+++ Lection3/Program.cs
@@ -0,0 +1,89 @@
+﻿//Console and Convert Basics
+/*Console.Write("Input Your name please: ");
+string myName = Console.ReadLine();
+
+Console.WriteLine($"User Input was: {myName}");
+
+
+string myString = "13";
+string myString2 = "7";
+
+Console.WriteLine(myString + myString2); //Concatenation
+
+
+Console.WriteLine("Please enter a number: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Please enter another number: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+int sum = firstNumber + secondNumber;
+
+Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is {sum}");
+*/
+
+//if statements
+int age = 30;
+
+if (age > 50)
+{
+    Console.WriteLine("Age is more then 50");
+}
+else
+{
+    Console.WriteLine("Age is 50 or less");
+}
+
+
+int speed = 100;
+
+if (speed < 60)
+{
+    Console.WriteLine("You are driving below the speed limit");
+}
+else if (speed >= 60 && speed <= 75)
+{
+    Console.WriteLine("You are driving within the speed limit");
+}
+else
+{
+    Console.WriteLine("You are driving above the speed limit");
+}
+
+
+int age2 = 25;
+bool hasLicense = false;
+
+if (age2 > 17 && hasLicense)
+{
+    Console.WriteLine("You can drive");
+}
+else if (age2 > 17 && !hasLicense)
+{
+    Console.WriteLine("You cannot drive, start stading");
+}
+else if (age2 < 17 && !hasLicense)
+{
+    Console.WriteLine("You cannot drive, you are too young");
+}
+else if (age2 < 17 && hasLicense)
+{
+    Console.WriteLine("Somehow You can drive, because you have a license");
+}
+else
+{
+    Console.WriteLine("You cannot drive, you are too young and you don't have a license");
+}
+
+
+decimal balanceOnAccount = 1000;
+decimal balanceOnCard = 3000;
+decimal price = 2500;
+
+if (balanceOnAccount > price || balanceOnCard > price || balanceOnAccount + balanceOnCard > price)
+{
+    Console.WriteLine("You can make the purchase");
+}
+else
+{
+    Console.WriteLine("You cannot make the purchase");
+}
---
+++ Lecture4/Program.cs
@@ -0,0 +1,189 @@
+﻿//Bank Console Imitation
+
+string correctUserName = "begsense";
+string correctPin = "1609";
+
+decimal balance = 1500;
+decimal limit = 50000;
+
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("Welcome to the Console Bank App!");
+Console.ResetColor();
+
+Console.WriteLine("================================");
+
+Console.WriteLine("Please enter your username:");
+string userName = Console.ReadLine();
+
+Console.WriteLine("Please enter your PIN:");
+string pin = Console.ReadLine();
+
+
+if (userName == correctUserName && pin == correctPin)
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("View Balance (1)");
+    Console.WriteLine("Transfer to Accounts (2)");
+    Console.WriteLine("Open Deposit (3)");
+    Console.WriteLine("Transfer to Mobile (4)");
+    Console.WriteLine("Transfer to Communal fees (5)");
+    Console.WriteLine("Loans (6)");
+    Console.ResetColor();
+
+    Console.WriteLine("Please select an option (1-6):");
+
+    string option = Console.ReadLine();
+
+    if (option == "1")
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Your current balance is: {balance} GEL");
+        Console.WriteLine($"Your current balance is: {balance / 2.7m} USD");
+        Console.WriteLine($"Your current balance is: {balance / 3.1m} EUR");
+        Console.ResetColor();
+    }
+    else if (option == "2")
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Enter the PID number to transfer to:");
+        string pidNumber = Console.ReadLine();
+
+        if (pidNumber.Length != 11)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid PID number. It must be 11 digits long.");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.WriteLine("Enter the amount to transfer:");
+            decimal transferAmount = decimal.Parse(Console.ReadLine());
+
+            if (transferAmount > balance || transferAmount <= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Insufficient funds or invalid amount. Transfer failed.");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Successfully transferred ${transferAmount} to PID {pidNumber}. New balance: ${balance - transferAmount}");
+                Console.ResetColor();
+            }
+        }
+    }
+    else if (option == "3")
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Enter the amount to deposit:");
+        decimal depositAmount = decimal.Parse(Console.ReadLine());
+
+        if (depositAmount <= 0 || depositAmount > limit)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid deposit amount. It must be greater than 0 and less than or equal to the limit.");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Successfully deposited ${depositAmount}. New balance: ${balance + depositAmount}");
+            Console.ResetColor();
+        }
+    }
+    else if (option == "4")
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Enter the mobile number to transfer to:");
+        string mobileNumber = Console.ReadLine();
+
+        if (mobileNumber == null)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid mobile number.");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.WriteLine("Enter the amount to transfer:");
+            decimal mobileTransferAmount = decimal.Parse(Console.ReadLine());
+            if (mobileTransferAmount > balance || mobileTransferAmount <= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Insufficient funds or invalid amount. Transfer failed.");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Successfully transferred ${mobileTransferAmount} to mobile number {mobileNumber}. New balance: ${balance - mobileTransferAmount}");
+                Console.ResetColor();
+            }
+        }
+    }
+    if (option == "5")
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Enter the amount to pay for communal fees:");
+        decimal communalAmount = decimal.Parse(Console.ReadLine());
+        if (communalAmount > balance || communalAmount <= 0)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Insufficient funds or invalid amount. Payment failed.");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Successfully paid ${communalAmount} for communal fees. New balance: ${balance - communalAmount}");
+            Console.ResetColor();
+        }
+    }
+    if ( option == "6")
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Enter the loan amount:");
+        decimal loanAmount = decimal.Parse(Console.ReadLine());
+        if (loanAmount <= 0 || loanAmount > limit)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid loan amount. It must be greater than 0 and less than or equal to the limit.");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Successfully granted a loan of ${loanAmount}. New balance: ${balance + loanAmount}");
+            Console.ResetColor();
+        }
+    }
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.BackgroundColor = ConsoleColor.Red;
+    Console.WriteLine("Incorrect username or PIN. Access denied.");
+    Console.ResetColor();
+}
