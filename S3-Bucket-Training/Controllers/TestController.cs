61b1f79a5830599dabf043c0988b93745a4d1f51
begsense <begsense@gmail.com>
2026-03-03 21:32:56 +0400

    add s3-bucket-working

---
+++ S3-Bucket-Training/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using S3_Bucket_Training.Helpers;
+
+namespace S3_Bucket_Training.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    private readonly S3Service _s3Service;
+
+    public TestController(S3Service s3Service)
+    {
+        _s3Service = s3Service;
+    }
+
+    [HttpPost("upload-test")]
+    public async Task<IActionResult> UploadTest(IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+        {
+            return BadRequest("No file uploaded.");
+        }
+        
+        var imageUrl = await _s3Service.UploadFileAsync(file);
+
+        return Ok(new { Url = imageUrl });
+    }
+}
---
+++ S3-Bucket-Training/Helpers/S3Service.cs
@@ -0,0 +1,42 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace S3_Bucket_Training.Helpers;
+
+public class S3Service
+{
+    private readonly IAmazonS3 _s3Client;
+    private readonly string _bucketName;
+    private readonly string _region;
+
+    public S3Service(IConfiguration config)
+    {
+        _bucketName = config["AWS:BucketName"];
+        _region = config["AWS:Region"];
+        var accessKey = config["AWS:AccessKey"];
+        var secretKey = config["AWS:SecretKey"];
+        _s3Client = new AmazonS3Client(accessKey, secretKey, Amazon.RegionEndpoint.GetBySystemName(_region));
+    }
+
+    public async Task<string> UploadFileAsync(IFormFile file, string folder = "uploads")
+    {
+        var fileName = $"{folder}/{Guid.NewGuid()}-{Path.GetExtension(file.FileName)}";
+
+        using var stream = file.OpenReadStream();
+
+        var request = new PutObjectRequest
+        {
+            BucketName = _bucketName,
+            Key = fileName,
+            InputStream = stream,
+            ContentType = file.ContentType,
+            CannedACL = S3CannedACL.PublicRead
+        };
+
+        await _s3Client.PutObjectAsync(request);
+
+        var url = $"https://{_bucketName}.s3.{_region}.amazonaws.com/{fileName}";
+
+        return url;
+    }
+}
